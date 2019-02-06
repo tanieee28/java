50aeea91470e4b70e719271ddede8bcefa06e2af
Tanmay Sharma <tanmay.sharma@xoriant.com>
2019-02-06 14:10:15 +0530

    CRUD operations basic

--- Samples/DatabaseAdapter/DatabaseAdapter/src/com/Adapter/MongoDB/MongoDBAdapter.java
+++ Samples/DatabaseAdapter/DatabaseAdapter/src/com/Adapter/MongoDB/MongoDBAdapter.java
@@ -1,10 +1,16 @@
 package com.Adapter.MongoDB;
 
+import com.mongodb.DBObject;
+import static com.mongodb.client.model.Filters.*;
+
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.ServerAddress;
 
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
+import com.mongodb.util.JSON;
 import com.mongodb.client.MongoCollection;
 
 import org.bson.Document;
@@ -26,6 +32,10 @@ public class MongoDBAdapter {
 	static MongoClient clientConnect;
 	MongoDatabase db;
 	MongoCollection<Document> coll;
+	
+	/*
+	 * Static Block for Connection Establishment to MongoDB
+	 */
 	static { 
 		try {
 			InputStream configFileStream   = MongoDBAdapter.class.getResourceAsStream("config.properties");
@@ -42,13 +52,96 @@ public class MongoDBAdapter {
 		
 	}
 	
+	/*
+	 * Constructor
+	 */
 	MongoDBAdapter(String dbName, String collName) {
+	
 		db = clientConnect.getDatabase(dbName);
 		coll = db.getCollection(collName);
+	
 	}
 	
-	public void insert(Document doc) {
-		coll.insertOne(doc);
+	/*
+	 * Insert One Operation
+	 */
+	public void insert(String jsonString) {
+	
+		//DBObject dbObj = (DBObject)JSON.parse(jsonString);
+		coll.insertOne(Document.parse(jsonString));
+	
 	}
-
+	
+	/*
+	 * Insert Many Operation
+	 */
+	public void insertMany(List<String> jsonStringList) {
+		
+		List<Document> documents = new ArrayList<Document>();
+		for (int i = 0; i < jsonStringList.size(); i++) {
+		    documents.add(Document.parse(jsonStringList.get(i)));
+		}
+		coll.insertMany(documents);
+		
+	}
+	
+	/*
+	 * Select All Operation
+	 */
+	public List<String> selectAll() {
+		
+		List<String> jsonStringList = new ArrayList<String>();
+		MongoCursor<Document> cursor = coll.find().iterator();
+		try {
+		    while (cursor.hasNext()) {
+		        System.out.println(cursor.next().toJson());
+		        jsonStringList.add( cursor.next().toJson());
+		    }
+		 
+		        
+		} finally {
+		   cursor.close();
+		}
+		return jsonStringList;
+		
+	}
+	
+	/*
+	 * Update Many Operation
+	 */
+	public void update(String condition, String fieldName,Object value,String jsonString) {
+		
+		UpdateResult result;
+		switch(condition) {
+			
+		case "eq":
+				result = coll.updateMany(eq(fieldName,value), new Document("$set", Document.parse(jsonString)));
+				System.out.println("Modified Count - "+result.getModifiedCount());
+				break;
+		
+		//WE CAN ADD MORE CONDITION DEPENDING ON REQUIREMENTS
+				
+		}	
+	}
+	
+	/*
+	 * Delete Many Operation
+	 */
+	public void delete(String condition, String fieldName,Object value) {
+	
+		DeleteResult result;
+		switch(condition) {
+		
+		case "eq":
+				result = coll.deleteMany(eq(fieldName,value));
+				System.out.println("Deleted Count - "+result.getDeletedCount());
+				break;
+		
+		//WE CAN ADD MORE CONDITION DEPENDING ON REQUIREMENTS
+				
+		}
+		
+	}
+	
+	
 }
--- Samples/DatabaseAdapter/DatabaseAdapter/src/com/Adapter/MongoDB/TempMain.java
+++ Samples/DatabaseAdapter/DatabaseAdapter/src/com/Adapter/MongoDB/TempMain.java
@@ -1,17 +1,68 @@
 package com.Adapter.MongoDB;
 
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import org.bson.Document;
 import com.Adapter.MongoDB.*;
+import com.mongodb.util.JSON;
 
 public class TempMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		Document doc = new Document("Company","Xoriant");
 		MongoDBAdapter adapter = new MongoDBAdapter("mydb", "mycoll");
-		adapter.insert(doc);
-		System.out.println("Transaction Completed");
+		Scanner scan = new Scanner(System.in);
+		Document doc = new Document("Company","Xoriant");
+		String jsonString = "{ 'EmpName' : 'Tanmay' }";
+				
+		while(true) {
+			
+			System.out.println("Specify 'I','R','U','D' for Insert,Read,Update,Delete \n \"STOP\" to exit");
+			String input = scan.next();
+			
+			switch(input) {
+			
+			case "R":
+				
+				List<String> recordsList = adapter.selectAll();
+				
+				for(String record:recordsList) {
+					System.out.println(record);
+				}
+				
+			break;
+				
+			case "U":
+				
+				adapter.update("eq", "Company", "Xoriant", "{'Company' : 'Xoriant Inc' }");
+				
+			break;
+				
+			case "I":
+				
+				adapter.insert(jsonString);
+				
+			break;
+			
+			case "D":
+				
+				adapter.delete("eq", "Company", "Xoriant Inc");
+				
+			break;
+			
+			case "STOP":
+				
+				System.exit(1);
+				
+			}
+			
+			System.out.println("Transaction Completed");
+		}
 	}
+	
+	
 
 }
